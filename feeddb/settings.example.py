0afa76ae8e0153d0403fb84e14d1f99b8ea38ae4
Xianhua Liu <xliu@nescent.org>
2009-08-07 02:33:19 +0000

    removed the settings file and added an example file
    git-svn-id: svn+ssh://svn.nescent.org/repos/projects/feeding/feeding_django/trunk@2798 7c3da866-2622-0410-982c-864fec618a66

--- feeddb/settings.example.py
+++ feeddb/settings.example.py
@@ -11,9 +11,9 @@ MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'feed_dev'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'xl24'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'feli20021122'         # Not used with sqlite3.
-DATABASE_HOST = 'darwin.nescent.org'             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
