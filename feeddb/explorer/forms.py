2cf668c5ef68ee3c708659628c5d632386ce1c09
Xianhua Liu <xliu@nescent.org>
2009-11-09 20:54:22 +0000

    initial version of form fo searching trials; implemented function for searching trials
    git-svn-id: svn+ssh://svn.nescent.org/repos/projects/feeding/feeding_django/trunk@2900 7c3da866-2622-0410-982c-864fec618a66

---
+++ feeddb/explorer/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from feeddb.feed.models import *
+
+SPECIES_CHOICES = [('','')]
+MUSCLE_CHOICES=[('','')]
+SENSOR_CHOICES=[]
+BEHAVIOR_CHOICES=[('','')]
+
+
+
+for s in Taxon.objects.all():
+    SPECIES_CHOICES.append((s.id, '%s %s' % (s.genus, s.species)))
+
+for s in Muscle.objects.all():
+    MUSCLE_CHOICES.append((s.id, s.label))
+
+for s in Technique.objects.all():
+    SENSOR_CHOICES.append((s.id, s.label))
+
+for s in Behavior.objects.all():
+    BEHAVIOR_CHOICES.append((s.id, s.label))
+
+
+class SearchTrailForm (forms.Form):
+    species = forms.ChoiceField(choices=SPECIES_CHOICES,required=False)
+    muscle = forms.ChoiceField(choices=MUSCLE_CHOICES,required=False)
+    #bone =forms.ChoiceField(required=False)
+    sensor = forms.MultipleChoiceField(choices=SENSOR_CHOICES,required=False)
+    primary_behavior=forms.ChoiceField(choices=BEHAVIOR_CHOICES,required=False)
+    food_type=forms.CharField(max_length=100,required=False)
+ 
