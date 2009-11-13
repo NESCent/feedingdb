8ab3793518620e618a854657ca41c653043d2aa8
Xianhua Liu <xliu@nescent.org>
2009-11-13 17:00:00 +0000

    added new tamplete tags for displaying unique list of techniques and muscles for trial
    git-svn-id: svn+ssh://svn.nescent.org/repos/projects/feeding/feeding_django/trunk@2904 7c3da866-2622-0410-982c-864fec618a66

---
+++ feeddb/explorer/templatetags/explorer_display.py
@@ -0,0 +1,31 @@
+from django.template import Library
+
+register = Library()
+
+def display_technique(trial):
+    """
+    Returns the unique techinique list for a trail.
+    """
+    techniques = set()
+    
+    for ch in trial.session.channels.all():
+        if not ch.setup.technique.label in techniques:
+            techniques.add(ch.setup.technique.label)
+    return ','.join(techniques)
+display_technique = register.simple_tag(display_technique)
+
+
+def display_muscle(trial):
+    """
+    Returns the unique muscles list for a trail.
+    """
+    musles = set()
+    
+    for ch in trial.session.channels.all():
+        for sensor in ch.setup.sensor_set.all():
+            if hasattr(sensor, 'emgsensor') and  not sensor.emgsensor.muscle.label in musles:
+                musles.add(sensor.emgsensor.muscle.label)
+            if hasattr(sensor, 'sonosensor') and not sensor.sonosensor.muscle.label in musles:
+                musles.add(sensor.sonosensor.muscle.label)
+    return ','.join(musles)
+display_muscle = register.simple_tag(display_muscle)
