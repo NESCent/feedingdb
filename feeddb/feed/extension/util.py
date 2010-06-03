d82e73745f006af073fa37a86e0f0f2ea08176c7
Xianhua Liu <xliu@nescent.org>
2010-06-03 18:36:00 +0000

    added new function for deep-copy object instance
    git-svn-id: svn+ssh://svn.nescent.org/repos/projects/feeding/feeding_django/trunk@3164 7c3da866-2622-0410-982c-864fec618a66

--- feeddb/feed/extension/util.py
+++ feeddb/feed/extension/util.py
@@ -7,6 +7,8 @@ from django.utils.encoding import force_unicode
 from django.utils.translation import ungettext, ugettext as _
 from django.core.urlresolvers import reverse, NoReverseMatch
 from django.contrib.admin.util import _nest_help, get_change_view_url
+from django.db.models.query import CollectedObjects
+from django.db.models.fields.related import *
 
 def get_feed_deleted_objects(deleted_objects, perms_needed, user, obj, opts, current_depth, admin_site, levels_to_root=4):
     """
@@ -127,3 +129,57 @@ def get_feed_deleted_objects(deleted_objects, perms_needed, user, obj, opts, cur
                 if hasattr(obj,"created_by"):
                     if obj.created_by != user:
                         perms_needed.add(related.opts.verbose_name)
+                        
+def duplicate(obj, exclude = None, value=None, field=None):
+    """
+    Duplicate all related objects of `obj` setting
+    `field` to `value`. If one of the duplicate
+    objects has an FK to another duplicate object
+    update that as well. Return the duplicate copy
+    of `obj`.  
+    """
+       
+    collected_objs = CollectedObjects()
+    obj._collect_sub_objects(collected_objs)
+    related_models = collected_objs.keys()
+    root_obj = None
+    # Traverse the related models in reverse deletion order.    
+    for model in reversed(related_models):
+        # Find all FKs on `model` that point to a `related_model`.
+        
+        fks = []
+        for f in model._meta.fields:
+            if isinstance(f, ForeignKey) and f.rel.to in related_models:
+                fks.append(f)
+        # Replace each `sub_obj` with a duplicate.
+        sub_obj = collected_objs[model]
+        for pk_val, obj in sub_obj.iteritems():
+            if hasattr(obj,"clone"):
+                obj= obj.clone(exclude)
+            else:
+                for fk in fks:
+                    fk_value = getattr(obj, "%s_id" % fk.name)
+                    # If this FK has been duplicated then point to the duplicate.
+                    if fk_value in collected_objs[fk.rel.to]:
+                        dupe_obj = collected_objs[fk.rel.to][fk_value]
+                        setattr(obj, fk.name, dupe_obj)
+            # Duplicate the object and save it.
+            if exclude !=None:
+                opts = obj._meta
+                model_name = opts.object_name
+                if not model_name in exclude:
+                    obj.id = None
+            else:
+                obj.id = None
+
+            if obj.id ==None:
+                if hasattr(obj, 'name'):
+                    setattr(obj,'name', "%s - cloned" % obj.name)
+                if hasattr(obj, 'title'):
+                    setattr(obj,'title', "%s - cloned" % obj.title)
+            if field!=None and value!=None:
+                setattr(obj, field, value)
+            obj.save()
+            if root_obj is None:
+                root_obj = obj
+    return root_obj
