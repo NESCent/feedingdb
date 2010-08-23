d251c9dc9f5cbe07de73eb41b5cfa2775278a946
Xianhua Liu <xliu@nescent.org>
2010-08-23 19:27:12 +0000

    added view for homepage
    git-svn-id: svn+ssh://svn.nescent.org/repos/projects/feeding/feeding_django/trunk@3408 7c3da866-2622-0410-982c-864fec618a66

--- feeddb/feed/views.py
+++ feeddb/feed/views.py
@@ -4,10 +4,4 @@ from django.shortcuts import render_to_response
 import models
 
 def index(request):
-    return HttpResponseRedirect("/admin/")
-
-def view_emgsetup(request, object_id):
-    return HttpResponse("viewing emgsetup")
-    
-def edit_emgsetup(request, object_id):
-    return HttpResponse("editing emgsetup")
+    return render_to_response("admin/home.html")
