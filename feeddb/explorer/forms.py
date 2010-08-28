0deba7ade4f94e4d695162545aba8f61682ced9d
Xianhua Liu <xliu@nescent.org>
2010-08-28 14:33:37 +0000

    changed technique from multiple choice to single select
    git-svn-id: svn+ssh://svn.nescent.org/repos/projects/feeding/feeding_django/trunk@3429 7c3da866-2622-0410-982c-864fec618a66

--- feeddb/explorer/forms.py
+++ feeddb/explorer/forms.py
@@ -22,11 +22,11 @@ for s in Behavior.objects.all():
     BEHAVIOR_CHOICES.append((s.id, s.label))
 
 
-class SearchTrailForm (forms.Form):
+class SearchTrialForm (forms.Form):
     species = forms.ChoiceField(choices=SPECIES_CHOICES,required=False)
     muscle = forms.ChoiceField(choices=MUSCLE_CHOICES,required=False)
     #bone =forms.ChoiceField(required=False)
-    sensor = forms.MultipleChoiceField(choices=SENSOR_CHOICES,required=False)
+    sensor = forms.ChoiceField(choices=SENSOR_CHOICES,required=False)
     primary_behavior=forms.ChoiceField(choices=BEHAVIOR_CHOICES,required=False)
     food_type=forms.CharField(max_length=100,required=False)
  
