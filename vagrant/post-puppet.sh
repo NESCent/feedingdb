f36960c00d06e0e9f174c8c79f6f764d3e1121e1
Evan Heidtmann <evan.heidtmann@gmail.com>
2014-07-16 15:31:55 -0700

    Update vagrant stuff

--- vagrant/post-puppet.sh
+++ vagrant/post-puppet.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 # post-puppet.sh: Add your shell commands here.
+
+yum groupinstall "Development Tools"
+yum install -y python-pip make gcc python-devel postgresql-devel postgresql-server
+service postgresql initdb
+service postgresql start
+
+easy_install virtualenv
+sudo -u vagrant virtualenv ~vagrant/venv
+source ~vagrant/venv/bin/activate
+pip install -r /server/src/feedb/dependencies.pip
