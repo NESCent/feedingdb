58e54325ac0c3cc985b79afa2bfe54a69153b0d4
Evan Heidtmann <evan.heidtmann@gmail.com>
2014-07-21 15:38:24 -0700

    Abstract out squishy_config/django.pp

--- vagrant/manifests/default.pp
+++ vagrant/manifests/default.pp
@@ -1,67 +1,11 @@
 include squishy_config::minimum
-include squishy_config::apache
 
 package { ['postgresql-devel']: ensure => present }
 
-class { 'postgresql::server':
-  manage_firewall => false,
-  postgres_password => 'postgres',
-}
-
-postgresql::server::db { 'feeddb':
-  user => 'feed',
-  password => postgresql_password('feed', 'feed'),
-}
-
-class { 'python':
-  version => 'system',
-  pip => true,
-  dev => true,
-  virtualenv => true,
-  gunicorn => false,
-}
-
-python::virtualenv { '/virtualenv/feed-venv':
-  owner => 'root',
-  group => 'apache',
-}
-
-python::requirements { '/server/src/feeddb/dependencies.pip':
-  virtualenv => '/virtualenv/feed-venv',
-  owner => 'root',
-  group => 'apache',
-  forceupdate => true,
-}
-
-class { 'apache::mod::wsgi':
-  wsgi_socket_prefix => '/var/run/wsgi',
-  wsgi_python_home => '/virtualenv/feed-venv',
-  wsgi_python_path => '/virtualenv/feed-venv/lib/python2.6',
-}
-
-apache::vhost { 'feeddb':
-  priority => '10',
-  port => '80',
-  docroot => '/server/htdocs',
-  wsgi_daemon_process => 'wsgi',
-  wsgi_daemon_process_options => {
-    processes => 2,
-    threads => 15,
-    display-name => '%{GROUP}',
-  },
-  wsgi_process_group => 'wsgi',
-  wsgi_script_aliases => {
-    '/' => '/server/src/feeddb/wsgi.py',
-  },
-  #aliases => [
-  #  { alias => '/media', path => '/server/src/feedb' TODO ... }
-  #],
-}
-
-# Ensure that apache::vhost doesn't clobber our docroot.
-# Alternately, you can use `link` or `directory` if you want to be explicit.
-file { '/server/htdocs':
-  ensure => present,
+squishy_config::django { 'feeddb':
+  pg_user => 'feed',
+  pg_pass => 'feed',
+  root => '/server/src/feeddb',
 }
 
 # insecure settings for ssh client within vagrant
---
+++ vagrant/modules/squishy_config/manifests/django.pp
@@ -0,0 +1,85 @@
+# requirements_force_update: set to `true` to force puppet to run `pip install -r ...` 
+define squishy_config::django(
+  $root = undef,
+  $pg_user   = $title,
+  $pg_pass   = $title,
+  $pg_host   = 'localhost',
+  $pg_name   = $title,
+  $virtualenv = "/virtualenv/$title/",
+  $requirements_file = undef,
+  $requirements_force_update = true,
+){
+  include squishy_config::apache
+
+  if ! $root {
+    fail("You must specify a Django installation root")
+  }
+
+  if ($requirements_file) {
+    $_requirements_file = $requirements_file
+  }
+  else {
+    $_requirements_file = "$root/requirements.txt"
+  }
+
+  # TODO: move class configuration to hiera and use `include` here.
+  class { 'postgresql::server':
+    manage_firewall => false,
+    postgres_password => 'postgres',
+  }
+
+  # TODO: move class configuration to hiera and use `include` here.
+  class { 'python':
+    version => 'system',
+    pip => true,
+    dev => true,
+    virtualenv => true,
+    gunicorn => false,
+  }
+
+  # TODO: make postgres optional; support mysql
+  postgresql::server::db { $pg_name:
+    user => $pg_user,
+    password => postgresql_password($pg_user, $pg_pass),
+  }
+
+  python::virtualenv { $virtualenv:
+    owner => 'root',
+    group => 'apache',
+  }
+
+  python::requirements { $_requirements_file:
+    virtualenv => $virtualenv,
+    owner => 'root',
+    group => 'apache',
+    forceupdate => $requirements_force_update,
+  }
+
+  # TODO: move class configuration to hiera and use `include` here.
+  class { 'apache::mod::wsgi':
+    wsgi_socket_prefix => '/var/run/wsgi',
+    wsgi_python_home => $virtualenv,
+  }
+
+  # TODO: figure out how to expose some of these to the caller. Perhaps an override
+  # hash and a create_resources call?
+  apache::vhost { $title:
+    priority => '10',
+    port => '80',
+    docroot => $root,
+    wsgi_daemon_process => 'wsgi',
+    wsgi_daemon_process_options => {
+      processes => 2,
+      threads => 15,
+      display-name => '%{GROUP}',
+    },
+    wsgi_process_group => 'wsgi',
+    wsgi_script_aliases => {
+      '/' => "$root/wsgi.py"
+    },
+  }
+
+  file { $root:
+    ensure => present,
+  }
+}
