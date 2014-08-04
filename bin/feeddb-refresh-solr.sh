e703dbb0619004c6ddf96542cf61727ca230974d
Evan Heidtmann <evan.heidtmann@gmail.com>
2014-08-04 13:54:57 -0700

    Fix refresh script to work on non-vagrant

--- bin/feeddb-refresh-solr.sh
+++ bin/feeddb-refresh-solr.sh
@@ -18,10 +18,19 @@ fi
 
 set -e
 
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
 echo "BEGIN building solr schema and restarting solr ..."
 
 source /virtualenv/feeddb/bin/activate
-/server/src/manage.py build_solr_schema > /etc/solr/conf/schema.xml 2> /dev/null
+$DIR/../src/manage.py build_solr_schema > /etc/solr/conf/schema.xml 2> /dev/null
 service solr restart &> /dev/null
 
 NEXT_WAIT_TIME=0
@@ -32,6 +41,6 @@ done
 echo "END building solr schema and restarting solr"
 echo "BEGIN rebuilding solr index ..."
 
-/server/src/manage.py rebuild_index --noinput
+$DIR/../src/manage.py rebuild_index --noinput
 
 echo "END rebuilding solr index"
