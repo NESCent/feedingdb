2bb91975c92bd9d9c235779426cba7b2d5201c08
Evan Heidtmann <evan.heidtmann@gmail.com>
2014-08-15 11:52:36 -0700

    Change search view to work with post, redirecting to canonical url

--- src/feeddb/feed/views.py
+++ src/feeddb/feed/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.utils.http import urlencode
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from django.conf import settings
 import models
 
 from haystack.views import FacetedSearchView
@@ -9,6 +12,8 @@ from forms import FeedSearchForm
 
 import logging
 logger = logging.getLogger(__name__)
+
+FACET_SORT_ORDER = getattr(settings, 'FACET_SORT_ORDER', [])
 
 def index(request):
     return render(request, "admin/home.html",{'user':request.user})
@@ -19,9 +24,47 @@ def about(request):
 def welcome(request):
     return render(request, "welcome.html",{'user':request.user})
 
+def filter_key(f):
+    """ See facet_key() in facets.py """
+    try:
+        index = FACET_SORT_ORDER.index(f[0])
+    except ValueError:
+        index = 0
+
+    return (index, f[0], f[1])
+
 class FeedSearchView(FacetedSearchView):
     def __init__(self):
         super(FeedSearchView, self).__init__(form_class=FeedSearchForm)
+
+    def __call__(self, request):
+        self.request = request
+
+
+        self.form = self.build_form()
+        if request.method == 'POST':
+            if request.POST.get('put_bucket', None):
+                from feeddb.explorer.views import trial_search_put
+                return trial_search_put(request)
+            else:
+                filters = self.form.searcher._clean_filters(request.POST)
+                filters = sorted(filters, key=filter_key)
+                q = request.POST.get('q', '')
+                per_page = request.POST.get('per_page', '')
+                if per_page:
+                    filters.insert(0, ('per_page', per_page) )
+                if q:
+                    filters.insert(0, ('q', q) )
+
+                url = reverse(self)
+                if len(filters):
+                    url += '?' + urlencode(filters)
+
+                return redirect(url)
+
+        self.query = self.get_query()
+        self.results = self.get_results()
+        return self.create_response()
 
     def _filter_facet_items(self, items, result_count=0):
         for item in items.items[:]:
@@ -37,7 +80,7 @@ class FeedSearchView(FacetedSearchView):
     def build_page(self):
         try:
             self.results_per_page = int(self.form.cleaned_data['per_page'])
-        except AttributeError: # cleaned_data doesn't exist
+        except (AttributeError, KeyError): # cleaned_data doesn't exist
             pass
 
         return super(FeedSearchView, self).build_page()
