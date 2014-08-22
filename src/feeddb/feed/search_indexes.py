b525c17b4360fdf0b9f964c91293a77e95dd67c5
Evan Heidtmann <evan.heidtmann@gmail.com>
2014-08-22 11:35:39 -0700

    Add freetext anatomical locations to indexing. Fixes BH-29
    Also renamed the fields to reflect the change

--- src/feeddb/feed/search_indexes.py
+++ src/feeddb/feed/search_indexes.py
@@ -6,12 +6,34 @@ import logging
 logger = logging.getLogger(__name__)
 
 def trial_muscles(obj):
+    """
+    Iterate over all EMG and Sono channels attached to a trial, yielding
+    MuscleOwls
+    """
     for channel in obj.session.channels.all():
         if hasattr(channel, 'emgchannel'):
             yield channel.emgchannel.sensor.muscle
         if hasattr(channel, 'sonochannel'):
-            for m in [channel.sonochannel.crystal1.muscle, channel.sonochannel.crystal2.muscle]:
-                yield m
+            yield channel.sonochannel.crystal1.muscle
+            yield channel.sonochannel.crystal2.muscle
+
+CHANNEL_TYPES_HAVING_LOCATION_FREETEXT = (
+    'strainchannel',
+    'forcechannel',
+    'pressurechannel',
+    'kinematicschannel',
+)
+
+def trial_analocs(obj):
+    """
+    Iterate over all channels having 'location_freetext" field which are
+    attached to a trial, yielding AnatomicalLocations
+    """
+    for channel in obj.session.channels.all():
+        for channeltype in CHANNEL_TYPES_HAVING_LOCATION_FREETEXT:
+            if hasattr(channel, channeltype):
+                typedchannel = getattr(channel, channeltype)
+                yield typedchannel.sensor.location_freetext
 
 def fail_with_return_value(ret):
     def wrap(f):
@@ -56,15 +78,19 @@ class TrialIndex(SearchIndex, Indexable):
 
     techniques = MultiValueField(indexed=False, stored=True, faceted=True)
 
-    # List of muscle labels for EMG and Sono sensors
-    muscles_direct = MultiValueField()
+    # List of muscle labels for EMG and Sono sensors and non-muscle anatomical
+    # locations on other sensors
+    analoc_direct = MultiValueField()
 
-    # `muscles_direct` along with each muscle's ancestors
-    muscles = MultiValueField(faceted=True)
+    # `analoc_direct` along with the ancestors of each MuscleOwl in the list
+    analoc = MultiValueField(faceted=True)
 
-    # Muscles which members of `muscles_direct` are a part of, except when the same
-    # muscle is already listed in `muscles`
-    muscles_part_of = MultiValueField(faceted=True)
+    # Muscles which members of `analoc_direct` are a part of, except when the same
+    # muscle is already listed in `analoc`
+    #
+    # This is currently just muscles, but could be extended to include any
+    # other OwlTerms if they are added later.
+    analoc_part_of = MultiValueField(faceted=True)
 
     def prepare_techniques(self, obj):
         technique_dict = dict(Techniques.CHOICES)
@@ -122,29 +148,42 @@ class TrialIndex(SearchIndex, Indexable):
         would be broadened by switching from one field to the other.
         """
 
-        muscles = set()
+        muscles_ancestors = set()
         muscles_part_of = set()
         muscles_direct = set()
         for m in trial_muscles(obj):
             if m != None and len(unicode(m)):
                 muscles_direct.add(unicode(m))
-                muscles.add(unicode(m))
                 for m_ancestor in m.ancestor_classes():
-                    muscles.add(unicode(m_ancestor))
+                    muscles_ancestors.add(unicode(m_ancestor))
                 for m_part_of in m.part_of_classes():
                     muscles_part_of.add(unicode(m_part_of))
 
-        # Only store muscles that the muscle is part of, but isn't already a
-        # subclass of
-        muscles_part_of = muscles_part_of.difference(muscles)
+        muscles_part_of = muscles_part_of.difference(muscles_ancestors)
 
         self.prepared_data = super(TrialIndex, self).prepare(obj)
         if settings.DEBUG:
-            print "MUSCLES %s" % muscles
+            print "MUSCLES DIRECT %s" % muscles_direct
+            print "MUSCLES ANCESTORS %s" % muscles_ancestors
             print "MUSCLES PART OF %s" % muscles_part_of
-        self.prepared_data['muscles'] = list(muscles) if len(muscles) else None
-        self.prepared_data['muscles_part_of'] = list(muscles_part_of) if len(muscles_part_of) else None
-        self.prepared_data['muscles_direct'] = list(muscles_direct) if len(muscles_direct) else None
+
+        # Now add the anatomical location terms to the set
+        analocs_direct = set()
+        for al in trial_analocs(obj):
+            if al != None and len(unicode(al)):
+                analocs_direct.add(unicode(al))
+
+        # Store all AnatomicalLocations, MuscleOwls, and ancestors of MuscleOwls
+        self.prepared_data['analoc'] = list(analocs_direct | muscles_direct | muscles_ancestors)
+
+        # Store muscles that the muscle is part of, but isn't already a
+        # subclass of. We don't have "part_of" relationships for analocs, so
+        # don't include them.
+        self.prepared_data['analoc_part_of'] = list(muscles_part_of - muscles_ancestors)
+
+        # Store MuscleOwls and AnatomicalLocations applied directly to the
+        # trial's session's channel's sensors.
+        self.prepared_data['analoc_direct'] = list(muscles_direct | analocs_direct)
         return self.prepared_data
 
     def load_all_queryset(self):
