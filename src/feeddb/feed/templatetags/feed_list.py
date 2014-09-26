4c577454520566d90ce1d047a2d9dc555b989c12
Shelly Furio <shelly@Shellys-MacBook-Pro.local>
2014-09-26 15:54:11 -0700

    Styled tables for consistency and standardized column widths

--- src/feeddb/feed/templatetags/feed_list.py
+++ src/feeddb/feed/templatetags/feed_list.py
@@ -247,7 +247,7 @@ def feed_result_list(context, cl):
     act =   {"text": "action",
                "sortable": False,
                "url": "",
-               "class_attrib": mark_safe(" class='action_header'")}
+               "class_attrib": mark_safe(" class='action_header2'")}
 
     header_list.append(act)
     return {'cl': cl,
