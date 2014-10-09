fd325d67e8a5e825c3826f73104f533ee906c5e7
Evan Heidtmann <evan.heidtmann@gmail.com>
2014-10-09 12:01:00 -0700

    Add more thoughts on cloning

--- src/feeddb/feed/management/commands/clone.py
+++ src/feeddb/feed/management/commands/clone.py
@@ -35,27 +35,49 @@ def print_clone(obj):
         # might take three saves, but eventually it will be updated to point to
         # the right session, experiment, and study.
 
+def _dict_by_attr(objects, attr):
+    return dict([(getattr(o, attr), o) for o in objects])
 
-def clone_session(session, experiment=None):
+def clone_experiment(experiment):
+    sessions = experiment.session_set.all()
+    setups = experiment.setup_set.all()
+    
+def clone_setup(setup):
+    # reverse foreign keys
+    channels = setup.channel_set.all()
+    sensors = setup.sensor_set.all()
+    sensors_by_old_id  = dict([(s.id, s) for s in sensors])
+    illustrations = setup.illustration_set.all()
+
+    _clone_basic(setup)
+
+    # trouble. each (typed) channel points to a typed sensor, and each sensor
+    # also points to the setup. Not every sensor has to be used by a channel.
+
+    for sensor in sensors:
+        _clone_basic(sensor, setup=setup)
+    for channel in channels:
+        # TODO: I need to do different things for each channel and sensor type.
+        # For example, SonoChannel objects have two sensors (crystal1 and
+        # crystal2) but all the rest have a single sensor. And the sensor is
+        # typed, so it probably doesn't work to assign an untyped sensor to the
+        # attribute.
+        if hasattr(channel, 'sonochannel'):
+            new_crystal1 = sensors_by_old_id[channel.sonochannel.crystal1]
+            new_crystal2 = sensors_by_old_id[channel.sonochannel.crystal2]
+            _clone_basic(channel, setup=setup, crystal1=new_crystal1, crystal2=new_crystal2)
+        else:
+            # FIXME: use typed sensor and typed channel here. i.e. channel.emgchannel.sensor
+            new_sensor = sensors_by_old_id[channel.sensor.id]
+            _clone_basic(channel, setup=setup, sensor=new_sensor)
+
+def clone_session(session):
     """
     Modifies its argument to become the new session
     """
-    copy_channels = True
-
-    if experiment is not None:
-        # we can't copy channels if we're changing experiment, because the channels
-        # are part of the setup which is part of the experiment.
-        #
-        # TODO: when we're copying an experiment, we need to be able to clone
-        # the channels while changing the experiment. Hrmph.
-        # (easy: change experiment before sending to this function!)
-        copy_channels = False
-        session.experiment = experiment
 
     trials = session.trial_set.all()
-    if copy_channels:
-        channels = session.channels.all()
-        channellineups = session.channellineup_set.all()
+    channellineups = session.channellineup_set.all()
 
     session.id = None
     session.pk = None
@@ -63,22 +85,18 @@ def clone_session(session, experiment=None):
     for trial in trials:
         clone_trial(trial, session=session)
 
-    if copy_channels:
-        # If we can copy channels, that means we're using the same setup, so we
-        # just need to copy the m2m relationships.
-        #
-        # Per Django docs, this would be a simple mapper of assigning the list
-        # of channels to the new m2m field, but we have a `through` table, so
-        # we have to make new relationships manually.
-        for lineup in channellineups:
-            clone_lineup(lineup, session=session)
+    # Per Django docs, this would be a simple mapper of assigning the list
+    # of channels to the new m2m field, but we have a `through` table, so
+    # we have to make new relationships manually.
+    for lineup in channellineups:
+        clone_lineup(lineup, session=session)
 
-def clone_trial(trial, session=None):
+def clone_trial(trial):
     trial.title = 'Cloned Trial (was "%s")' % trial.title
-    _clone_basic(trial, session=session)
+    _clone_basic(trial)
 
-def clone_lineup(lineup, session=None):
-    _clone_basic(lineup, session=session)
+def clone_lineup(lineup):
+    _clone_basic(lineup)
 
 def _clone_basic(thing, **kwargs):
     thing.id = None
