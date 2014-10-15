9abec06aee9c161a5868afc4ae3c824be6313290
Evan Heidtmann <evan.heidtmann@gmail.com>
2014-10-15 15:13:36 -0700

    Fix BH-395: use qs args to update status in session

--- src/feeddb/feed/middleware.py
+++ src/feeddb/feed/middleware.py
@@ -1,5 +1,13 @@
 from feeddb.feed.util import FeedUploadStatus
+from django.http import HttpResponseRedirect
 
 class FeedUploadStatusMiddleware():
     def process_request(self, request):
         request.feed_upload_status = FeedUploadStatus(session=request.session)
+        changed_params = request.feed_upload_status.update_with_querystring(request.GET)
+        if changed_params:
+            qs = changed_params.urlencode()
+            if len(qs):
+                return HttpResponseRedirect(request.path + '?' + qs)
+            else:
+                return HttpResponseRedirect(request.path)
