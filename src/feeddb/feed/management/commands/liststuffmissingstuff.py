db5409172d18411093448702c78f908f72dd72b9
Evan Heidtmann <evan.heidtmann@gmail.com>
2014-10-16 16:26:18 -0700

    Fix TypeError in previous commit

--- src/feeddb/feed/management/commands/liststuffmissingstuff.py
+++ src/feeddb/feed/management/commands/liststuffmissingstuff.py
@@ -9,23 +9,27 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print "Trials missing OWL behavior:"
         for t in trials_missing_behaviorowl():
+            print "  %d: %s"
             try:
-                print "  %d: %s"
-                print "      behavior_primary: '%s' pk=%d" % (t.id, t, t.behavior_primary, hasattr(t.behavior_primary, 'pk') and t.behavior_primary.pk)
-                print "  %s" % (t.get_absolute_url())
+                pk = t.behavior_primary.pk
             except AttributeError:
-                pass
+                pk = 0
+            print "      behavior_primary: '%s' pk=%d" % (t.id, t, t.behavior_primary, pk)
+            print "  %s" % (t.get_absolute_url())
         else:
             print "  -- 0 trials -- "
 
         print "Sensors missing muscle:"
         for typename, s in sensors_missing_muscle():
+            print "  %d: %s (%s)" % (s.id, s, typename)
             try:
-                print "  %d: %s (%s)" % (s.id, s, typename)
-                print "      location_controlled: '%s' (pk=%d)" % (s.location_controlled, hasattr(s.location_controlled, 'pk') and s.location_controlled.pk)
-                print "  %s" % (s.get_absolute_url())
-            except AttributeError:
-                pass
+                pk = s.location_controlled.pk
+            except:
+                pk = 0
+            print "      location_controlled: '%s' (pk=%d)" % (s.location_controlled, pk)
+            print "  %s" % (s.get_absolute_url())
+        else:
+            print "  -- 0 sensors --"
 
 def trials_missing_behaviorowl():
     return Trial.objects.filter(behaviorowl_primary__isnull=True).select_related('behavior_primary')
