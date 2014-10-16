65dc408f1c87d56624c993f9dd45c29207be9eab
Evan Heidtmann <evan.heidtmann@gmail.com>
2014-10-16 16:23:44 -0700

    Avoid traceback when listing stuff

--- src/feeddb/feed/management/commands/liststuffmissingstuff.py
+++ src/feeddb/feed/management/commands/liststuffmissingstuff.py
@@ -9,17 +9,23 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print "Trials missing OWL behavior:"
         for t in trials_missing_behaviorowl():
-            print "  %d: %s"
-            print "      behavior_primary: '%s' pk=%d" % (t.id, t, t.behavior_primary, t.behavior_primary.pk)
-            print "  %s" % (t.get_absolute_url())
+            try:
+                print "  %d: %s"
+                print "      behavior_primary: '%s' pk=%d" % (t.id, t, t.behavior_primary, hasattr(t.behavior_primary, 'pk') and t.behavior_primary.pk)
+                print "  %s" % (t.get_absolute_url())
+            except AttributeError:
+                pass
         else:
             print "  -- 0 trials -- "
 
         print "Sensors missing muscle:"
         for typename, s in sensors_missing_muscle():
-            print "  %d: %s (%s)" % (s.id, s, typename)
-            print "      location_controlled: '%s' (pk=%d)" % (s.location_controlled, s.location_controlled.pk)
-            print "  %s" % (s.get_absolute_url())
+            try:
+                print "  %d: %s (%s)" % (s.id, s, typename)
+                print "      location_controlled: '%s' (pk=%d)" % (s.location_controlled, hasattr(s.location_controlled, 'pk') and s.location_controlled.pk)
+                print "  %s" % (s.get_absolute_url())
+            except AttributeError:
+                pass
 
 def trials_missing_behaviorowl():
     return Trial.objects.filter(behaviorowl_primary__isnull=True).select_related('behavior_primary')
