27e14911aaf33f4cde7045f7b27f19a67293aaa9
Evan Heidtmann <evan.heidtmann@gmail.com>
2014-10-22 13:56:32 -0700

    Fix behavior list. Big oops!

--- src/feeddb/feed/models.py
+++ src/feeddb/feed/models.py
@@ -140,8 +140,6 @@ class BehaviorOwl(OwlTerm):
 
     @staticmethod
     def default_qs_filter_args():
-        # TODO: fix the restrictions below based on feedback from client
-        return dict(rdfs_is_class=True)
         return dict(
             rdfs_subClassOf_ancestors__uri=u'http://purl.obolibrary.org/obo/OPBO_0000012'
 
