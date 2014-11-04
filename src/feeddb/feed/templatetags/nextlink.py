19f37086cafcdc31873d92de6a5edc20fd1b84fd
Evan Heidtmann <evan.heidtmann@gmail.com>
2014-11-04 13:25:36 -0800

    Fix BH-637: redirect to study after deletion unless directed otherwise

--- src/feeddb/feed/templatetags/nextlink.py
+++ src/feeddb/feed/templatetags/nextlink.py
@@ -27,5 +27,19 @@ def next_url(context):
     '''
     Returns empty string if cannot be found
     '''
-    return  querystring_url(context, 'next')
+    return querystring_url(context, 'next')
 
+@register.simple_tag(takes_context=True)
+def next_or_study_url(context):
+    """
+    Returns the value of next_url() or, if undefined, the current study URL if
+    known.
+    """
+    try:
+        import upload_status
+        containers = upload_status.get_current_containers(context)
+        study_url = containers['study'].get_absolute_url()
+    except:
+        study_url = False
+
+    return querystring_url(context, 'next', default=study_url)
