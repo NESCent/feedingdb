cca095196fce867d36a891cb86cd8f67eae760f2
Victor Rocha <victor@rochapps.com>
2015-12-30 23:36:38 -0500

    [django1.8] Create new migration for django

--- src/feeddb/explorer/migrations/0001_initial.py
+++ src/feeddb/explorer/migrations/0001_initial.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('feed', '__first__'),
+        ('feed', '0001_initial'),
     ]
 
     operations = [
